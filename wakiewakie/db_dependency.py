a229b39553fcb9253ec8e30e0869ace6ac3bc5b0
Dan Joar Heimvik <danjoar@gmail.com>
2023-09-29 08:03:59 +0200

    Added support for .env

--- wakiewakie/db_dependency.py
+++ wakiewakie/db_dependency.py
@@ -1,5 +1,12 @@
 import psycopg
 from psycopg.rows import dict_row
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
 class DbDependency:
     conn: psycopg.AsyncConnection = None
@@ -7,7 +14,7 @@ class DbDependency:
     def __init__(self) -> None:
         pass
     async def start(self):
-        self.conn = await psycopg.AsyncConnection.connect(dbname="wakiewakie", user="postgres", password="abc123", host="localhost", port="5432")
+        self.conn = await psycopg.AsyncConnection.connect(f"dbname={POSTGRES_DB} user={POSTGRES_USER} password={POSTGRES_PASSWORD} host=localhost port=5432")
     async def __call__(self):
         cur = self.conn.cursor(row_factory=dict_row)
         try:
